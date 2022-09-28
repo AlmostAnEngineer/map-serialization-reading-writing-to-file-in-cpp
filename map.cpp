c62e588379119db1961114c71af57bbc52087fcc
AlmostAnEngineer <94685050+AlmostAnEngineer@users.noreply.github.com>
2022-09-29 00:23:49 +0200

    Some cosmetics to code

--- map.cpp
+++ map.cpp
@@ -1,21 +1,39 @@
 #include <unordered_map>
 #include <fstream>
 #include <iostream>
-
+#include <filesystem>
 struct someData
 {
     std::string dataname;
 };
+using Map = std::unordered_map<int, someData>;
+void printData(Map &map);
+void readData(Map &map);
+void addData(Map &map, int ID, std::string param);
+void saveData(Map &map);
 
 int main()
 {
-    using Map = std::unordered_map<int, someData>; // we gonna use it :)
     Map map;
-
-    /*
-                    READING DATA
-    */
-    if (std::filesystem::exists("data.dat")) //If file exist we gonna read the data.
+    readData(map);
+    for (size_t i = 4; i < 1000000; i++)
+    {
+        addData(map, i, "name");
+    }
+    printData(map);
+    saveData(map);
+    return 0;
+}
+void printData(Map &map)
+{
+    for (auto [key, value] : map)
+    {
+        std::cout << key << " " << value.dataname << std::endl;
+    }
+}
+void readData(Map &map)
+{
+    if (std::filesystem::exists("data.dat")) // If file exist we gonna read the data.
     {
         std::ifstream instream;
         instream.open("data.dat");
@@ -35,17 +53,15 @@ int main()
             exit(1);
         }
     }
-    /*
-    Adding some custom data and reading it
-    */
-    map[1].dataname = std::string("someName");
-    for (auto [key, value] : map)
-    {
-        std::cout << key << " " << value.dataname << std::endl;
-    }
-    /*
-         SAVE TO FILE
-    */
+}
+
+void addData(Map &map, int ID, std::string param)
+{
+    map[ID].dataname = param;
+}
+
+void saveData(Map &map)
+{
     std::ofstream streamdata;
     streamdata.open("data.dat");
     if (streamdata.is_open())
